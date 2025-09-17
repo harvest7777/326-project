ffc98020505f3b667ad5c25a7fb6d47d94b58d3f
harvest7777 <333harvest.moon333@gmail.com>
2025-09-17 16:03:10 -0700

    broilerplate

--- linux/filecopy.c
+++ linux/filecopy.c
@@ -14,9 +14,22 @@
 #define READ_END 0
 #define WRITE_END 1
 
+/**
+ * Main Function: The main function will parse command-line arguments, create a pipe, fork a child
+ * process, and handle file copying.
+ */
 int main(int argc, char *argv[])
 {
     // Minimal hello world
     printf("Hello, World!\n");
     return 0;
+}
+
+/**
+ * File Copying Logic: This section will contain the logic for reading from the source file and writing
+ * to the pipe in the parent process, as well as reading from the pipe and writing to the destination file
+ * in the child process.
+ */
+void file_copy(const char *source, const char *destination) {
+	return;
 }
